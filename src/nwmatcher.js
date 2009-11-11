a3db1def52a84fff0ba20baa430f741e791fe305
jdalton <john.david.dalton@gmail.com>
2009-11-10 23:34:19 -0600

    nwmatcher: Add wildcard `*` tagName processing to the `RE_SIMPLE_SELECTOR` regexp for non-IE browsers. [Diego Perini]

--- src/nwmatcher.js
+++ src/nwmatcher.js
@@ -354,9 +354,9 @@ NW.Dom = (function(global) {
     true,
 
   // matches simple id, tagname & classname selectors
-  RE_SIMPLE_SELECTOR = BUGGY_GEBCN
+  RE_SIMPLE_SELECTOR = BUGGY_GEBTN || BUGGY_GEBCN
     ? /^#?[-\w]+$/
-    : /^[.#]?[-\w]+$/,
+    : /^[.#*]?[-\w]*$/,
 
   /*----------------------------- LOOKUP OBJECTS -----------------------------*/
 
