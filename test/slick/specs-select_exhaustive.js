090b859c2c88455f3fe442783c57397647024ae9
jdalton <john.david.dalton@gmail.com>
2009-11-19 02:53:33 -0600

    unittest: Fix invalid className in Slick `exhaustive` tests. [jddalton]

--- test/slick/specs-select_exhaustive.js
+++ test/slick/specs-select_exhaustive.js
@@ -1,6 +1,6 @@
 function specsSelectorExhaustive(specs,context){
-	
-	var CLASSES = "normal escaped\\,character \u01dd\u0070\u006f\u0254\u0131\u0075\u006e \u7021 with-dash with_underscore 123number MiXeDcAsE".split(' ');
+	// Converted characters to unicode and renamed `123number` to `number123` because it is an invalid className. (jddalton)
+	var CLASSES = "normal escaped\\,character \u01dd\u0070\u006f\u0254\u0131\u0075\u006e \u7021 with-dash with_underscore number123 MiXeDcAsE".split(' ');
 	Describe('CLASS',function(specs,context){
 		
 		specs.before_each = function(){
